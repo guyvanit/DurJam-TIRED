0f3a4ddcc86703b6d6b6da92e2a62b2231649a6c
Guy <premsupapong@gmail.com>
2020-08-28 15:23:51 +0700

    marked unused arguments

--- src/ClickButton.gd
+++ src/ClickButton.gd
@@ -4,7 +4,7 @@ class_name ClickButton
 #func _physics_process(delta):
 	# add animation so button moves up and down (do later)
 
-func _input_event(viewport, event, shape_idx):
+func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == BUTTON_LEFT \
 	and event.is_pressed():
