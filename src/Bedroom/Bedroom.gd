f453d1c1f78a61578c7ad4ee41235f840cc775bf
Guy <premsupapong@gmail.com>
2020-08-29 21:09:04 +0700

    turn lights

---
+++ src/Bedroom/Bedroom.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+func _ready():
+	var dark = get_node("Bedroom_dark")
+	print(GlobalScript.bedroom_lights)
+	if GlobalScript.bedroom_lights:
+		dark.hide()
+	else:
+		dark.show()
--- src/Bedroom/button_light.gd
+++ src/Bedroom/button_light.gd
@@ -2,3 +2,14 @@ extends ClickButton
 
 func on_click():
 	print("Light Button")
+	var dark = get_node("../Bedroom_dark")
+	#print(GlobalScript.bedroom_lights)
+	
+	# if the light is on, turn the light off (enable dark)
+	if GlobalScript.bedroom_lights:
+		dark.show()
+		GlobalScript.bedroom_lights = false
+	# if the light is off, turn light on (disbale dark)
+	else:
+		dark.hide()
+		GlobalScript.bedroom_lights = true
